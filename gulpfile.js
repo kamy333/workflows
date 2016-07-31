c970f0736cb866b201864e292685fc2562876375
Kamran Nafisspour <nafisspour@bluewin.ch>
2016-08-01 00:47:33 +0200

    Added browserifyt task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@
  * Created by Kamran on 31-Jul-16.
  */
 
-gulp=require('gulp');
-gutil=require('gulp-util');
-coffee=require('gulp-coffee');
+var gulp=require('gulp'),
+gutil=require('gulp-util'),
+coffee=require('gulp-coffee'),
+concat=require('gulp-concat'),
+ browserify=require('gulp-browserify');
+
 
 var coffeeSources=['components/coffee/tagline.coffee'];
+var jsSources='components/scripts/*.js';
 
 gulp.task('log',function () {
     gutil.log('I just love gulp!');
@@ -18,4 +22,11 @@ gulp.task('coffee',function () {
         .pipe(coffee({bare:true}))
         .on('error',gutil.log)
         .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js',['coffee'],function () {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(browserify())
+        .pipe(gulp.dest('builds/development/js'))
 });
